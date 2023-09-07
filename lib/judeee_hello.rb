36f462a651d3e5155f47ef471e1c754c3b74972e
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-09-07 13:51:40 +0900

    initial commit

---
+++ lib/judeee_hello.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "judeee_hello/version"
+
+module JudeeeHello
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/judeee_hello_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JudeeeHello do
+  it "has a version number" do
+    expect(JudeeeHello::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
